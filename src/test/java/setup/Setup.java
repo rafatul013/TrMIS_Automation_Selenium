b25c66b33e434760c71b9c38de4e25a6e76adf97
rafatul <rafatulislam@inneed.cloud>
2024-01-18 14:33:42 +0600

    test4

--- src/test/java/setup/Setup.java
+++ src/test/java/setup/Setup.java
@@ -22,7 +22,7 @@ public class Setup {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
-            options.setHeadless(false);
+            options.setHeadless(true);
 
             WebDriverManager.chromedriver().setup();
             Driver = new ChromeDriver(options);
